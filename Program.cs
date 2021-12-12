2106af84c16be0260be7db5e47410946cc089dc2
Nikita <falkorickman@gmail.com>
2021-12-12 16:43:24 +0100

    Add comment for rectangular

--- Program.cs
+++ Program.cs
@@ -21,40 +21,55 @@ namespace Test
             Console.WriteLine("_________________________________\n");
 
 
-            //Určuje délky stran pomocí Pythagorovy věty
+            //Určuje délky 3 stran pomocí Pythagorovy věty
             double AB = Math.Sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2));
-            double AC= Math.Sqrt((x1 - x3) * (x1 - x3) + (y1 - y3) * (y1 - y3));
-            double BC= Math.Sqrt((x2 - x3) * (x2 - x3) + (y2 - y3) * (y2 - y3));
+            double AC = Math.Sqrt((x1 - x3) * (x1 - x3) + (y1 - y3) * (y1 - y3));
+            double BC = Math.Sqrt((x2 - x3) * (x2 - x3) + (y2 - y3) * (y2 - y3));
 
 
             //Zavádí dvě proměnné, které budou použity v dalších výpočtech
-            double Square =0;
+            double Square = 0;
             double Perimetr;
-            
+
             if (AB + AC > BC && AC + BC > AB && BC + AB > AC)
             {
-                Console.WriteLine("Triangle exist and has 3 points:\n");                            
+                //Vypisuje souradnice trech pointu
+                Console.WriteLine("Triangle exist and has 3 points:\n");
                 Console.WriteLine($"Point A ({x1},{y1})");
                 Console.WriteLine($"Point B ({x2},{y2})");
                 Console.WriteLine($"Point C ({x3},{y3})");
+
                 Console.WriteLine("_________________________________\n");
+
+                //Výpočet obvodu trojúhelníku
                 Perimetr = (AB + AC + BC);
-                Perimetr= Math.Round(Perimetr, 2);
+                Perimetr = Math.Round(Perimetr, 2);
                 Console.WriteLine($"Perimeter is\t{Perimetr}");
+
+                //Výpočet plochy trojúhelníku
                 double s = (AB + AC + BC) / 2;
                 Square = Math.Sqrt(s * (s - AB) * (s - AC) * (s - BC));
                 Square = Math.Round(Square, 2);
                 Console.WriteLine($"Square is \t{Square}");
                 Console.WriteLine("_________________________________\n");
+
+
+
                 AB = Math.Round(AB, 2);
                 AC = Math.Round(AC, 2);
                 BC = Math.Round(BC, 2);
+
+
                 Console.WriteLine($"AB side has \t{AB} points");
                 Console.WriteLine($"AC side has \t{AC} points");
                 Console.WriteLine($"BC side has \t{BC} points");
                 Console.WriteLine("_________________________________\n");
 
+
+
                 double Hypotinuse = Math.Max(AB, Math.Max(AC, BC));
+
+
                 double katet1 = 0;
                 double katet2 = 0;
                 if (AB != Hypotinuse) { katet1 = AB; }
@@ -65,15 +80,16 @@ namespace Test
                 }
                 if (BC != Hypotinuse) { katet2 = BC; }
 
-                if ((katet1 * katet2)/2 == Square)
+                if ((katet1 * katet2) / 2 == Square)
                 {
                     Console.WriteLine("This triangle is rectangular");
                 }
+                else Console.WriteLine("This triangle is not rectangular");
             }
             else
             {
-                Console.WriteLine("Triangle doesn't exist");               
-            }                                  
+                Console.WriteLine("Triangle doesn't exist");
+            }
             Console.ReadLine();
         }
     }
